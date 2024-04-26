445220bc6a8fa0a45dd366e2a8a4ec1be4f769b8
Xavier DESPUJOLS <x.despujols@seelink.fr>
2024-04-26 17:33:52 +0200

    Update record_securitymeasure.form.php

--- front/record_securitymeasure.form.php
+++ front/record_securitymeasure.form.php
@@ -4,7 +4,7 @@
  GDPR Records of Processing Activities plugin for GLPI
  Copyright (C) 2020 by Yild.
 
- https://github.com/yild/gdprropa
+ https://github.com/xdespujols/rgpd
  -------------------------------------------------------------------------
 
  LICENSE
@@ -30,27 +30,26 @@
 
  --------------------------------------------------------------------------
 
-  @package   gdprropa
-  @author    Yild
+  @package   rgpd
+  @author    XDespujols
   @copyright Copyright (c) 2020 by Yild
   @license   GPLv3+
              http://www.gnu.org/licenses/gpl.txt
-  @link      https://github.com/yild/gdprropa
+  @link      https://github.com/xdespujols/rgpd
   @since     2020
  --------------------------------------------------------------------------
  */
-
 include("../../../inc/includes.php");
 
 Session::checkLoginUser();
 
 if (isset($_POST['add'])
-   && (isset($_POST['plugin_gdprropa_records_id']) && ($_POST['plugin_gdprropa_securitymeasures_id']))) {
+   && (isset($_POST['plugin_rgpd_records_id']) && ($_POST['plugin_rgpd_securitymeasures_id']))) {
 
-   $record = new PluginGdprropaRecord();
-   $record->check($_POST[PluginGdprropaRecord::getForeignKeyField()], UPDATE);
+   $record = new PluginRgpdRecord();
+   $record->check($_POST[PluginRgpdRecord::getForeignKeyField()], UPDATE);
 
-   $item = new PluginGdprropaRecord_SecurityMeasure();
+   $item = new PluginRgpdRecord_SecurityMeasure();
    $item->add($_POST);
 
 }
