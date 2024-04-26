71fcda6461077b8b6970d07c246329440fbd8eb8
Xavier DESPUJOLS <x.despujols@seelink.fr>
2024-04-26 17:36:56 +0200

    Update controllerinfo.class.php

--- inc/controllerinfo.class.php
+++ inc/controllerinfo.class.php
@@ -4,7 +4,7 @@
  GDPR Records of Processing Activities plugin for GLPI
  Copyright (C) 2020 by Yild.
 
- https://github.com/yild/gdprropa
+ https://github.com/xdespujols/rgpd
  -------------------------------------------------------------------------
 
  LICENSE
@@ -30,12 +30,12 @@
 
  --------------------------------------------------------------------------
 
-  @package   gdprropa
-  @author    Yild
+  @package   rgpd
+  @author    XDespujols
   @copyright Copyright (c) 2020 by Yild
   @license   GPLv3+
              http://www.gnu.org/licenses/gpl.txt
-  @link      https://github.com/yild/gdprropa
+  @link      https://github.com/xdespujols/rgpd
   @since     2020
  --------------------------------------------------------------------------
  */
@@ -44,7 +44,7 @@ if (!defined('GLPI_ROOT')) {
    die("Sorry. You can't access this file directly");
 }
 
-class PluginGdprropaControllerInfo extends CommonDBChild {
+class PluginRgpdControllerInfo extends CommonDBChild {
 
    static public $itemtype = 'Entity';
    static public $items_id = 'entities_id';
@@ -52,23 +52,23 @@ class PluginGdprropaControllerInfo extends CommonDBChild {
    static public $logs_for_parent = true;
    static public $checkParentRights = true;
 
-   static $rightname = 'plugin_gdprropa_controllerinfo';
+   static $rightname = 'plugin_rgpd_controllerinfo';
 
    static function getTypeName($nb = 0) {
 
-      return __("GDPR Controller Info", 'gdprropa');
+      return __("GDPR Controller Info", 'rgpd');
    }
 
    function getTabNameForItem(CommonGLPI $item, $withtemplate = 0) {
 
-      if (!PluginGdprropaControllerInfo::canView()) {
+      if (!PluginRgpdControllerInfo::canView()) {
          return false;
       }
 
       switch ($item->getType()) {
          case Entity::class :
 
-            return self::createTabEntry(PluginGdprropaControllerInfo::getTypeName(), 0);
+            return self::createTabEntry(PluginRgpdControllerInfo::getTypeName(), 0);
       }
 
       return '';
@@ -117,24 +117,24 @@ class PluginGdprropaControllerInfo extends CommonDBChild {
 
       $canedit = $this->can($this->fields['id'], UPDATE);
 
-      if ($this->fields['id'] <= 0 && !PluginGdprropaControllerInfo::canCreate()) {
-         echo "<br><br><span class='b'>" . __("Controller information not set.", 'gdprropa') . "</span><br><br>";
+      if ($this->fields['id'] <= 0 && !PluginRgpdControllerInfo::canCreate()) {
+         echo "<br><br><span class='b'>" . __("Controller information not set.", 'rgpd') . "</span><br><br>";
 
          return;
       }
 
       $options['canedit'] = $canedit;
-      $options['formtitle'] = __("Manage entity Controller information", 'gdprropa');
+      $options['formtitle'] = __("Manage entity Controller information", 'rgpd');
 
       $this->initForm($this->fields['id'], $options);
       $this->showFormHeader($options);
 
       echo "<tr class='tab_bg_3'><td colspan='4'><center><strong>";
-      echo __("GDPR Article 30 1a", 'gdprropa');
+      echo __("GDPR Article 30 1a", 'rgpd');
       echo "</strong></center></td></tr>";
 
       echo "<tr class='tab_bg_2'><td width='$colsize1'>";
-      echo __("Legal representative", 'gdprropa');
+      echo __("Legal representative", 'rgpd');
       echo "</td><td width='$colsize2'>";
       User::dropdown([
          'right' => 'all',
@@ -143,7 +143,7 @@ class PluginGdprropaControllerInfo extends CommonDBChild {
       ]);
 
       echo "</td><td width='$colsize3'>";
-      echo __("Data Protection Officer", 'gdprropa');
+      echo __("Data Protection Officer", 'rgpd');
       echo "</td><td  width='$colsize4'>";
       User::dropdown([
          'right' => 'all',
@@ -153,7 +153,7 @@ class PluginGdprropaControllerInfo extends CommonDBChild {
       echo "</td></tr>";
 
       echo "</td><td width='$colsize1'>";
-      echo __("Controller Name", 'gdprropa');
+      echo __("Controller Name", 'rgpd');
       echo "</td><td colspan='3'>";
       if ($this->fields['id'] <= 0) {
          $this->fields['controllername'] = '';
@@ -163,11 +163,11 @@ class PluginGdprropaControllerInfo extends CommonDBChild {
       echo "</td></tr>";
 
       echo "<tr class='tab_bg_3'><td colspan='4'><center><strong>";
-      echo __("Controller configuration - contract types", 'gdprropa');
+      echo __("Controller configuration - contract types", 'rgpd');
       echo "</strong></center></td></tr>";
 
       echo "<tr class='tab_bg_2'><td width='$colsize1'>";
-      echo __("Joint Controller Contract Type", 'gdprropa');
+      echo __("Joint Controller Contract Type", 'rgpd');
       echo "</td><td width='$colsize2'>";
       ContractType::dropdown([
          'width' => '75%',
@@ -175,7 +175,7 @@ class PluginGdprropaControllerInfo extends CommonDBChild {
          'value' => array_key_exists('contracttypes_id_jointcontroller', $this->fields) ? $this->fields['contracttypes_id_jointcontroller'] : null
       ]);
       echo "</td><td width='$colsize3'>";
-      echo __("Processor Contract Type", 'gdprropa');
+      echo __("Processor Contract Type", 'rgpd');
       echo "</td><td width='$colsize4'>";
       ContractType::dropdown([
          'width' => '75%',
@@ -185,7 +185,7 @@ class PluginGdprropaControllerInfo extends CommonDBChild {
       echo "</td></tr>";
 
       echo "<tr class='tab_bg_2'><td width='$colsize1'>";
-      echo __("Thirdparty Contract Type", 'gdprropa');
+      echo __("Thirdparty Contract Type", 'rgpd');
       echo "</td><td width='$colsize2'>";
       ContractType::dropdown([
          'width' => '75%',
@@ -193,7 +193,7 @@ class PluginGdprropaControllerInfo extends CommonDBChild {
          'value' => array_key_exists('contracttypes_id_thirdparty', $this->fields) ? $this->fields['contracttypes_id_thirdparty'] : null
       ]);
       echo "</td><td width='$colsize3'>";
-      echo __("Internal Contract Type", 'gdprropa');
+      echo __("Internal Contract Type", 'rgpd');
       echo "</td><td width='$colsize4'>";
       ContractType::dropdown([
          'width' => '75%',
@@ -203,7 +203,7 @@ class PluginGdprropaControllerInfo extends CommonDBChild {
       echo "</td></tr>";
 
       echo "<tr class='tab_bg_2'><td width='$colsize1'>";
-      echo __("Other Contract Type", 'gdprropa');
+      echo __("Other Contract Type", 'rgpd');
       echo "</td><td width='$colsize2'>";
       ContractType::dropdown([
          'width' => '75%',
@@ -222,24 +222,24 @@ class PluginGdprropaControllerInfo extends CommonDBChild {
       $this->showFormButtons($options);
       Html::closeForm();
 
-      if ((Session::haveRight('plugin_gdprropa_createpdf', CREATE))) {
+      if ((Session::haveRight('plugin_rgpd_createpdf', CREATE))) {
          echo "<div class='firstbloc'>";
-         echo "<form method='GET' action=\"" . $CFG_GLPI['root_doc'] . "/plugins/gdprropa/front/createpdf.php\">";
+         echo "<form method='GET' action=\"" . $CFG_GLPI['root_doc'] . "/plugins/rgpd/front/createpdf.php\">";
          echo "<table class='tab_cadre_fixe' id='mainformtable'>";
          echo "<tbody>";
          echo "<tr class='headerRow'>";
-         echo "<th colspan='3' class=''>" . __('PDF creation settings', 'gdprropa') . "</th>";
+         echo "<th colspan='3' class=''>" . __('PDF creation settings', 'rgpd') . "</th>";
          echo "</tr>";
 
-         $_config = PluginGdprropaCreatePDF::getDefaultPrintOptions();
-         $_config['report_type'] = PluginGdprropaCreatePDF::REPORT_FOR_ENTITY;
-         PluginGdprropaCreatePDF::showConfigFormElements($_config);
+         $_config = PluginRgpdCreatePDF::getDefaultPrintOptions();
+         $_config['report_type'] = PluginRgpdCreatePDF::REPORT_FOR_ENTITY;
+         PluginRgpdCreatePDF::showConfigFormElements($_config);
 
          echo "</table>";
-         echo "<input type='hidden' name='report_type' value=\"" . PluginGdprropaCreatePDF::REPORT_FOR_ENTITY . "\">";
+         echo "<input type='hidden' name='report_type' value=\"" . PluginRgpdCreatePDF::REPORT_FOR_ENTITY . "\">";
          echo "<input type='hidden' name='entities_id' value='" . $entity->fields['id'] . "'>";
          echo "<input type='hidden' name='action' value=\"print\">";
-         echo "<input type='submit' class='submit' name='createpdf' value='" . __("Create Controller RoPA PDF for Entity", 'gdprropa') . "' />";
+         echo "<input type='submit' class='submit' name='createpdf' value='" . __("Create Controller RoPA PDF for Entity", 'rgpd') . "' />";
          Html::closeForm();
          echo "</div>";
       }
@@ -250,7 +250,7 @@ class PluginGdprropaControllerInfo extends CommonDBChild {
 
    static function getFirstControllerInfo($entity_id, $allow_from_ancestors = true) {
 
-      $controllerinfo = new PluginGdprropaControllerInfo();
+      $controllerinfo = new PluginRgpdControllerInfo();
       $controllerinfo->getFromDBByCrit(['entities_id' => $entity_id]);
 
       if (!$allow_from_ancestors) {
@@ -264,7 +264,7 @@ class PluginGdprropaControllerInfo extends CommonDBChild {
          } else {
             $ancestors = getAncestorsOf('glpi_entities', $entity_id);
             foreach (array_reverse($ancestors) as $ancestor) {
-               return PluginGdprropaControllerInfo::getFirstControllerInfo($ancestor);
+               return PluginRgpdControllerInfo::getFirstControllerInfo($ancestor);
             }
          }
       }
@@ -272,7 +272,7 @@ class PluginGdprropaControllerInfo extends CommonDBChild {
 
    static function getContractTypes($entity_id, $compact = false) {
 
-      $controllerinfo = PluginGdprropaControllerInfo::getFirstControllerInfo($entity_id, PluginGdprropaConfig::getConfig('system', 'allow_controllerinfo_from_ancestor'));
+      $controllerinfo = PluginRgpdControllerInfo::getFirstControllerInfo($entity_id, PluginRgpdConfig::getConfig('system', 'allow_controllerinfo_from_ancestor'));
 
       $out = [
          'contracttypes_id_jointcontroller' => -1,
@@ -304,7 +304,7 @@ class PluginGdprropaControllerInfo extends CommonDBChild {
          'table' => 'glpi_users',
          'field' => 'name',
          'linkfield' => 'users_id_representative',
-         'name' => __("Legal representative", 'gdprropa'),
+         'name' => __("Legal representative", 'rgpd'),
          'massiveaction' => false,
          'datatype' => 'dropdown',
          'joinparams' => [
@@ -322,7 +322,7 @@ class PluginGdprropaControllerInfo extends CommonDBChild {
          'table' => 'glpi_users',
          'field' => 'name',
          'linkfield' => 'users_id_dpo',
-         'name' => __("Data Protection Officer", 'gdprropa'),
+         'name' => __("Data Protection Officer", 'rgpd'),
          'massiveaction' => false,
          'datatype' => 'dropdown',
          'joinparams' => [
@@ -339,7 +339,7 @@ class PluginGdprropaControllerInfo extends CommonDBChild {
          'id' => '5603',
          'table' => self::getTable(),
          'field' => 'controllername',
-         'name' => __("Controller Name", 'gdprropa'),
+         'name' => __("Controller Name", 'rgpd'),
          'massiveaction' => false,
          'joinparams' => [
             'jointype' => 'child'
@@ -357,7 +357,7 @@ class PluginGdprropaControllerInfo extends CommonDBChild {
          'id' => '11',
          'table' => $this->getTable(),
          'field' => 'controllername',
-         'name' => __("Controller Name", 'gdprropa'),
+         'name' => __("Controller Name", 'rgpd'),
          'massiveaction' => false,
          'datatype' => 'text',
       ];
